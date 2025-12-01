988b96c160f6cae3b8ba7225abd79c23280e7513
Adi3366 <adityabhaskar3636@gmail.com>
2025-12-01 15:08:04 +0530

    Fix formatting issues in Day14_Q28.c

--- Day14_Q28.c
+++ Day14_Q28.c
@@ -2,16 +2,17 @@
 
 int main() {
     int n;
-    long long product = 1;   // Use long long for large results
+    long long product = 1;
 
     printf("Enter value of n: ");
     scanf("%d", &n);
 
     for (int i = 2; i <= n; i += 2) {
-        product *= i;   // multiply even numbers only
+        product *= i;
     }
 
     printf("Product of even numbers from 1 to %d = %lld\n", n, product);
 
     return 0;
 }
+
