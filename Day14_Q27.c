5b530027214b732d5e70c29c223b312b1b2bb42e
Adi3366 <adityabhaskar3636@gmail.com>
2025-12-01 15:07:17 +0530

    Fix formatting issues in Day14_Q27.c

--- Day14_Q27.c
+++ Day14_Q27.c
@@ -7,11 +7,12 @@ int main() {
     scanf("%d", &n);
 
     for (int i = 1; i <= n; i++) {
-        sum += (2 * i - 1);   // i-th odd number = 2i - 1
+        sum += (2 * i - 1);
     }
 
     printf("Sum of first %d odd numbers = %d\n", n, sum);
 
     return 0;
 }
+
 
